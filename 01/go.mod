1b4f32872140bc717f03aedaba2a71c236d23a33
Adel Sehic <adel@bicomsystems.com>
2025-12-02 10:15:26 +0100

    - Day 1 solved, pretty stupid way of solving part 2 but oh well

---
+++ 01/go.mod
@@ -0,0 +1,7 @@
+module main
+
+go 1.24.1
+
+require github.com/AdelSehic/advent-of-code-2025/helpers v0.0.0-20251202082844-aa04075192aa
+
+replace github.com/AdelSehic/advent-of-code-2025/helpers => ../helpers
--- 01/main.go
+++ 01/main.go
@@ -1,3 +1,87 @@
 package main
 
-import "github.com/AdelSehic/advent-of-code-2025/helpers"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/AdelSehic/advent-of-code-2025/helpers"
+)
+
+func part1() int {
+	buff := helpers.NewBuffer[int]()
+
+	for i := range 100 {
+		buff.Enqueue(&i)
+	}
+	buff.SetIndex(50)
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	counter := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		str := scanner.Text()
+		dir := string(str[0])
+		move, err := strconv.Atoi(str[1:])
+		if err != nil {
+			fmt.Println(err.Error())
+			panic(err)
+		}
+		if dir == "R" {
+			buff.MoveRight(move)
+		} else {
+			buff.MoveLeft(move)
+		}
+		if *buff.Get() == 0 {
+			counter++
+		}
+	}
+	return counter
+}
+
+func part2() int {
+	buff := helpers.NewBuffer[int]()
+
+	for i := range 100 {
+		buff.Enqueue(&i)
+	}
+	buff.SetIndex(50)
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	counter := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		str := scanner.Text()
+		dir := string(str[0])
+		move, err := strconv.Atoi(str[1:])
+		if err != nil {
+			fmt.Println(err.Error())
+			panic(err)
+		}
+		moveFunc := buff.Next
+		if dir == "L" {
+			moveFunc = buff.Previous
+		}
+		for range move {
+			moveFunc()
+			if *buff.Get() == 0 {
+				counter++
+			}
+		}
+	}
+	return counter
+}
+
+func main() {
+	fmt.Println("Part 1:", part1())
+	fmt.Println("Part 2:", part2())
+}
